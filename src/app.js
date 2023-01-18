6da179c2928c9725d83d8f36d7b5350b96da0dfa
ElizabethMaciasSandoval <elizabeth130394@gmail.com>
2023-01-18 19:29:43 -0300

    :sparkles: se agrega la clase ProductManager y las rutas get

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 import express from "express";
+import { ProductManager } from "./manager/ProductManager.js";
+
+const manager = new ProductManager('src/files/products.json')
 
 const puerto = 8080;
 const app = express();
 
+app.get('/products', async (req, res) => {
+  const products = await manager.getProducts();
+  const { limit } = req.query
+  if(limit){
+    const productsLimit = products.slice(0, Number(limit))
+    return res.send(productsLimit)
+  }else{
+    return res.send(products)
+  }
+})
+
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const productsById = await manager.getProductById(Number(id));
+  res.send(productsById);
+})
+
 app.listen(puerto, ()=>{
-    console.log(`server listening port: ${puerto}`)
+  console.log(`server listening port: ${puerto}`)
 })
